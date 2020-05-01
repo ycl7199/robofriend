9972b07f56b0bdaddce786ec9082263db09c0895
劉益誠 <kevin861006@gmail.com>
2020-05-01 16:02:53 +0800

    testing

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,6 +17,7 @@ class App extends Component {
         
     }
 
+    
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
